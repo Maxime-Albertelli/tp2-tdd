6943cbe09778f08e8a185477eec5c45c8cc29f9d
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2022-03-22 11:53:40 +0100

    Correct some frenglishism

--- src/test/java/fr/univ_amu/iut/exercice2/FizzBuzzTest.java
+++ src/test/java/fr/univ_amu/iut/exercice2/FizzBuzzTest.java
@@ -11,93 +11,90 @@ public class FizzBuzzTest {
     @Disabled
     @Test
     public void test_computeString_Should_Return_1_when_1() {
-        String resultat = fizzBuzz.computeString(1);
-        assertEquals("1", resultat);
+        String result = fizzBuzz.computeString(1);
+        assertEquals("1", result);
     }
 
     @Disabled
     @Test
     public void test_computeString_Should_Return_2_when_2() {
-        String resultat = fizzBuzz.computeString(2);
-        assertEquals("2", resultat);
+        String result = fizzBuzz.computeString(2);
+        assertEquals("2", result);
     }
 
     @Disabled
     @Test
     public void test_computeString_Should_Return_Fizz_when_3() {
-        String resultat = fizzBuzz.computeString(3);
-        assertEquals("Fizz", resultat);
+        String result = fizzBuzz.computeString(3);
+        assertEquals("Fizz", result);
     }
-    
+
     @Disabled
     @Test
     public void test_computeString_Should_Return_4_when_4() {
-        String resultat = fizzBuzz.computeString(4);
-        assertEquals("4", resultat);
+        String result = fizzBuzz.computeString(4);
+        assertEquals("4", result);
     }
 
     @Disabled
     @Test
     public void test_computeString_Should_Return_Buzz_when_5() {
-        String buzz = "Buzz";
-        String resultat = fizzBuzz.computeString(5);
-        assertEquals("Buzz", buzz);
+        String result = fizzBuzz.computeString(5);
+        assertEquals("Buzz", result);
     }
 
     @Disabled
     @Test
     public void test_computeString_Should_Return_Fizz_when_6() {
-        String resultat = fizzBuzz.computeString(6);
-        assertEquals("Fizz", resultat);
+        String result = fizzBuzz.computeString(6);
+        assertEquals("Fizz", result);
     }
 
     @Disabled
     @Test
     public void test_computeString_Should_Return_Fizz_when_9() {
-        String resultat = fizzBuzz.computeString(9);
-        assertEquals("Fizz", resultat);
+        String result = fizzBuzz.computeString(9);
+        assertEquals("Fizz", result);
     }
 
     @Disabled
     @Test
     public void test_computeString_Should_Return_Buzz_when_10() {
-        String resultat = fizzBuzz.computeString(10);
-        assertEquals("Buzz", resultat);
+        String result = fizzBuzz.computeString(10);
+        assertEquals("Buzz", result);
     }
 
     @Disabled
     @Test
     public void test_computeString_Should_Return_FizzBuzz_when_15() {
-        String resultat = fizzBuzz.computeString(15);
-        assertEquals("FizzBuzz", resultat);
+        String result = fizzBuzz.computeString(15);
+        assertEquals("FizzBuzz", result);
     }
 
     @Disabled
     @Test
     public void test_computeList_Should_Return_a_sequence_of_5_elements_when_5() {
-        String[] resultat = fizzBuzz.computeList(5);
-        assertEquals(5, resultat.length);
+        String[] result = fizzBuzz.computeList(5);
+        assertEquals(5, result.length);
     }
 
     @Disabled
     @Test
     public void test_computeList_Should_Return_a_sequence_of_5_FizzBuzz_elements_when_5() {
-        String[] resultat = fizzBuzz.computeList(5);
-        assertEquals("1", resultat[0]);
-        assertEquals("2", resultat[1]);
-        assertEquals("Fizz", resultat[2]);
-        assertEquals("4", resultat[3]);
-        assertEquals("Buzz", resultat[4]);
+        String[] result = fizzBuzz.computeList(5);
+        assertEquals("1", result[0]);
+        assertEquals("2", result[1]);
+        assertEquals("Fizz", result[2]);
+        assertEquals("4", result[3]);
+        assertEquals("Buzz", result[4]);
     }
 
     @Disabled
     @Test
     public void test_computeList_Should_Return_a_sequence_of_15_FizzBuzz_elements_when_15() {
-        String[] resultat = fizzBuzz.computeList(15);
-        int i = 0;
-        for (String number : resultat) {
-            assertEquals(fizzBuzz.computeString(i + 1), resultat[i]);
-            i++;
+        String[] result = fizzBuzz.computeList(15);
+        for (int i = 0; i < result.length; ++i) {
+            assertEquals(fizzBuzz.computeString(i + 1), result[i]);
         }
     }
 }
